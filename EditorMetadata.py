e5991c440f9dc63faa5c6ac7149663cc98bc8016
NeNorm1s <maks121005sl@yandex.ru>
2024-09-19 21:14:03 +0300

    начало

--- EditorMetadata.py
+++ EditorMetadata.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk, filedialog, messagebox
@@ -17,10 +18,9 @@ class AppMetadataEditor(tk.Tk):
         file_type = StringVar(value=file_types[0])
         file_name = StringVar()
 
-        audios = Variable()
+        audios = Variable(value=[])
         audio_selected = Variable()
 
-        dir_state = Variable()
         dir_files = Variable()
 
         # Метаданные
@@ -38,22 +38,79 @@ class AppMetadataEditor(tk.Tk):
         ckb_md_track_number = BooleanVar()
         ckb_md_title = BooleanVar()
 
-        def Dir(com):
-            match com:
-                case "search":
-                    return filedialog.askdirectory()
+        # Адаптация под действия
+        def ReloadWidgets():
+            def state_machine(widgets, state):
+                for widget in widgets.winfo_children():
+                    try:
+                        widget['state'] = state
+                    except TclError:
+                        pass
+            try:
+                os.listdir(dir_files.get())
+            except FileNotFoundError:
+                state_machine(frm_file, tk.DISABLED)
+            else:
+                state_machine(frm_file, tk.NORMAL)
 
+        def TypingPathDir(*args):
+            Select()
+
+        def Select_type(event):
+            Select()
+
+        def Select_file(event):
+            pass
+
+        def Select():
+            # Обновление списка аудио файлов
+            def list_update():
+                arr = []
+                try:
+                    for i in os.listdir(dir_files.get()):
+                        if i.endswith(file_type.get()):
+                            arr.append(i)
+                    audios.set(arr)
+                    ReloadWidgets()
+                except FileNotFoundError:
+                    ReloadWidgets()
+
+            list_update()
+
+        # <<-- Создание виджетов -->>
         frm_dir = ttk.Frame(self)
+        frm_file = ttk.Frame(self)
         frm_metadata = ttk.Frame(self)
 
+        lsb_dir_audios = Listbox(frm_dir, listvariable=audios)
         lbl_dir_files = ttk.Label(frm_dir, text="Директория:")
         ent_dir_files = ttk.Entry(frm_dir, textvariable=dir_files)
-        btn_dir_files = ttk.Button(frm_dir, text="...", command=lambda: dir_files.set(Dir("search")))
+        btn_dir_files = ttk.Button(frm_dir, text="...", command=lambda: dir_files.set(filedialog.askdirectory()))
+        lbl_dir_type = ttk.Label(frm_dir, text="Расширение:")
+        cmb_dir_type = ttk.Combobox(frm_dir, textvariable=file_type, values=file_types, width=5)
+
+        lbl_file_title = ttk.Label(frm_file, text="Название:")
+        ent_file_title = ttk.Entry(frm_file, textvariable=file_name)
+
+        dir_files.trace('w', TypingPathDir)
+
+        cmb_dir_type.bind('<<ComboboxSelected>>', Select_type)
+        lsb_dir_audios.bind('<<ListboxSelect>>', Select_file)
 
         # <<-- Расположение виджетов -->>
-        #
+        # Фрейм директории
         frm_dir.grid(row=0, column=0)
-
         lbl_dir_files.grid(row=0, column=0)
         ent_dir_files.grid(row=0, column=1)
         btn_dir_files.grid(row=0, column=2)
+        lbl_dir_type.grid(row=1, column=0)
+        cmb_dir_type.grid(row=1, column=1)
+        lsb_dir_audios.grid(row=0, column=3, rowspan=2, columnspan=2)
+        # Фрейм файла
+        frm_file.grid(row=1, column=0)
+        lbl_file_title.grid(row=0, column=0)
+        ent_file_title.grid(row=0, column=1)
+
+        frm_metadata.grid(row=1, column=1)
+
+        ReloadWidgets()
